c756cf854013716a94cb7f3872826329b389f3a6
Jeongyeon Park <jungyy808@hanyang.ac.kr>
2025-11-26 16:24:01 +0900

    course

--- backend/consultations/urls.py
+++
@@ -1,9 +0,0 @@
-# backend/consultations/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.consultation_list_create_api),
-    path("instructors/", views.instructor_list_api),
-    path("<int:pk>/", views.consultation_detail_api), # [추가] 상세/수정/삭제 URL
-]
--- backend/user/utils.py
+++
@@ -1,8 +0,0 @@
-# backend/user/utils.py
-import string
-import random
-
-def generate_random_password(length=10):
-    """10자리 랜덤 임시 비밀번호 생성"""
-    chars = string.ascii_letters + string.digits + "!@#$"
-    return ''.join(random.choice(chars) for _ in range(length))
